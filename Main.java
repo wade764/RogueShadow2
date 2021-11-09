799cf10a9b19aa36e1fa92b7e37772c1d3eb9580
Wade Nelson <2manypuppiez467@gmail.com>
2021-11-09 10:31:43 -0500

    Fixed main method syntax

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@
 import ansi_terminal.*;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         // put termain in raw mode
         Terminal.rawMode();
 
