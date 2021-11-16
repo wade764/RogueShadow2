ba8ab985626eb4d341f60f58c685039ebfdc23d2
Wade Nelson <2manypuppiez467@gmail.com>
2021-11-16 13:03:23 -0500

    unpaused title screen stuff and added a line to the title

--- Game.java
+++ Game.java
@@ -71,31 +71,36 @@ public class Game {
         System.out.print("<                                                                           >");
         Terminal.warpCursor(16, 36);//40
         System.out.print("*   You the adventurer must set forth into the dark world of Rogue Shadow.  *");
-        Terminal.pause(1.5);
+        //Terminal.pause(1.5);
         Terminal.warpCursor(17, 36);
         System.out.print("<                                                                           >");
         Terminal.warpCursor(18, 36);//38
         System.out.print("* A land of mystical wonder and danger presents itself around every corner. *");
-        Terminal.pause(1.5);
+        //Terminal.pause(1.5);
         Terminal.warpCursor(19, 36);
         System.out.print("<                                                                           >");
         Terminal.warpCursor(20, 36);//38
         System.out.print("* The goal of the game is to survive and collect as many items as possible. *");
-        Terminal.pause(1.5);
+        //Terminal.pause(1.5);
         Terminal.warpCursor(21, 36);
         System.out.print("<                                                                           >");
         Terminal.warpCursor(22, 36);//46
-        System.out.print("*         Beware of the dungeons enemies or you will surely perish!         *");
+        System.out.print("*         To advance to the next floor you must kill all the enemies.       *");
+        //Terminal.pause(1.5);
         Terminal.warpCursor(23, 36);
         System.out.print("<                                                                           >");
-        Terminal.warpCursor(24, 36);
+        Terminal.warpCursor(24, 36);//46
+        System.out.print("*         Beware of the dungeons enemies or you will surely perish!         *");
+        Terminal.warpCursor(25, 36);
+        System.out.print("<                                                                           >");
+        Terminal.warpCursor(26, 36);
         System.out.print("*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*v*");
 
         //setting the players name
-        Terminal.warpCursor(26, 58);
+        Terminal.warpCursor(28, 58);
         System.out.print("What is your name adventurer? ");
         Scanner in = new Scanner(System.in);
-        Terminal.warpCursor(26, 88);
+        Terminal.warpCursor(28, 88);
         // initializing the name of the player
         name = in.next();
         player.setName(name);
