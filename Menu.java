086375425541e66194ee786c149f0dda4fe5ebaa
Wade Nelson <2manypuppiez467@gmail.com>
2021-11-18 08:38:44 -0500

    Working on a Menu class

---
+++ Menu.java
@@ -0,0 +1,49 @@
+import ansi_terminal.Terminal
+
+public class Menu {
+
+    void titleScreen() {
+        System.out.print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        System.out.print("<*************************************************************************>");
+        System.out.print("<*-----------------------------------------------------------------------*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*|                                                                     |*>");
+        System.out.print("<*-----------------------------------------------------------------------*>");
+        System.out.print("<*************************************************************************>");
+        System.out.print("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv");
+
+    }
+}
