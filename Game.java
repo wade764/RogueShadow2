c6d049bd8f32519adee7cb775f6982041fd52e0f
Megan <mmeiser19@gmail.com>
2021-11-18 20:09:09 -0500

    gergy5g

--- Game.java
+++ Game.java
@@ -337,8 +337,8 @@ public class Game {
                     boxSize = in.nextInt();
                     in.nextLine();
                     player = new Player(in);
-                    /*in.nextLine();
                     in.nextLine();
+                    /*in.nextLine();
                     in.nextLine();
                     in.nextLine();*/
                     for (int i = 0; i < enemySize; i++) { //read in enemies on current floor
