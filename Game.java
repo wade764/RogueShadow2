d2192e2296bd31c7cd32606129711228124d2428
Megan <mmeiser19@gmail.com>
2021-11-18 20:04:46 -0500

    hihih

--- Game.java
+++ Game.java
@@ -339,6 +339,8 @@ public class Game {
                     player = new Player(in);
                     in.nextLine();
                     in.nextLine();
+                    in.nextLine();
+                    in.nextLine();
                     for (int i = 0; i < enemySize; i++) { //read in enemies on current floor
                         enemies.set(i, new Enemy(in));
                     }
