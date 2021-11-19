068eb08575d729bddd94fa54ac88906518a8a948
Megan <mmeiser19@gmail.com>
2021-11-19 08:01:10 -0500

    test

--- Entity.java
+++ Entity.java
@@ -112,8 +112,8 @@ public class Entity {
     }
 
     public Entity (Scanner in) {
-        in.nextLine();
-        in.nextLine();
+        //in.nextLine();
+        //in.nextLine();
         int row = in.nextInt();
         int col = in.nextInt();
         position = new Position(row, col);
