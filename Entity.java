5952a2140989d64fe5862f063963b3c641c2fcbd
Wade Nelson <2manypuppiez467@gmail.com>
2021-11-20 19:26:51 -0500

    HELLO I WISH I COULD SOLVE THISgit pull!

--- Entity.java
+++ Entity.java
@@ -115,15 +115,24 @@ public class Entity {
         out.println(color);
     }
 
-    public Entity (Scanner in) {
+    public Entity(Scanner in) {
 
         in.nextLine(); //skips line in file that says Entity
         int row = in.nextInt(); // have issues reading this in
-        in.nextLine();//this is needed to consume the remaining white space on the current line
+        
+        //TESTING
+         Terminal.clear();
+         Terminal.warpCursor(25,0);
+         System.out.print("Hey I am in line 126 of Entity(Scanner) The line you are reading in is: "+row);
+         Terminal.pause(4);
 
-        Terminal.warpCursor(2, 0);
+
+        in.nextLine();//this is needed to consume the remaining white space on the current line
+        
+        //TESTING
+        /*Terminal.warpCursor(2, 0);
         System.out.print("Row: " + row);
-        Terminal.pause(2);
+        Terminal.pause(2);*/
 
         int col = in.nextInt();
         in.nextLine();//this is needed to consume the remaining white space on the current line
@@ -152,6 +161,11 @@ public class Entity {
             case "BLACK" -> color = Color.BLACK;
             case "WHITE" -> color = Color.WHITE;
         }
+        //TESTING
+         Terminal.clear();
+         Terminal.warpCursor(12,0);
+         System.out.print("Hi me again, I am in line 167 of Entity(Scanner) The line you are reading in is: "+c);
+         Terminal.pause(4);
 
         /*Terminal.warpCursor(5, 0);
           System.out.print("Color: " + color);
