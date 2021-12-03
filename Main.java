7950a634b16bc873692e798b8ba7cded4af1ab18
Wade Nelson <2manypuppiez467@gmail.com>
2021-12-03 10:51:04 -0500

    Added a javadoc comment in Main

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@
 // contains the main class for running the game
 
 import ansi_terminal.*;
-
+/**
+* The following program is a game developed for CPSC 240
+* @author WN,MM,AB 
+*/
 public class Main {
     public static void main(String[] args) {
         // put terminal in raw mode
